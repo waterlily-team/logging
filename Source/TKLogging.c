2fb794c673128d3aef97c671aaf4a76c590f8835
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-18 15:12:42 -0400

    added basic logging function

--- Source/TKLogging.c
+++ Source/TKLogging.c
@@ -1,1 +1,29 @@
 #include <TKLogging.h>
+#include <stdarg.h>
+
+static FILE *getProperStream(rpgtk_log_type_t type)
+{
+    switch (type)
+    {
+        case RPGTK_LOG_TYPE_VERBOSE:     [[fallthrough]];
+        case RPGTK_LOG_TYPE_INFORMATION: return stdout;
+        case RPGTK_LOG_TYPE_WARNING:     [[fallthrough]];
+        case RPGTK_LOG_TYPE_ERROR:       return stderr;
+    }
+    // We can't really ever get here, but GCC whines and it's also good
+    // insurance if I forget to update the switch with any new log types.
+    return stdout;
+}
+
+void(rpgtk_log)(rpgtk_log_metadata_t data, rpgtk_log_type_t type,
+                FILE *redirect, const char *const format, ...)
+{
+    if (redirect == nullptr) redirect = getProperStream(type);
+
+    (void)data;
+
+    va_list args;
+    va_start(args, format);
+    vfprintf(redirect, format, args);
+    va_end(args);
+}
--- TKLogging.h
+++ TKLogging.h
@@ -1,6 +1,28 @@
 #ifndef TKLOGGING_MAIN_H
 #define TKLOGGING_MAIN_H
 
-void placeholder(void);
+#include <stdio.h>
+
+typedef enum
+{
+    RPGTK_LOG_TYPE_VERBOSE,
+    RPGTK_LOG_TYPE_INFORMATION,
+    RPGTK_LOG_TYPE_WARNING,
+    RPGTK_LOG_TYPE_ERROR
+} rpgtk_log_type_t;
+
+typedef struct
+{
+    const char *const file;
+    const char *const function;
+    const size_t line;
+} rpgtk_log_metadata_t;
+
+void(rpgtk_log)(rpgtk_log_metadata_t data, rpgtk_log_type_t type,
+                FILE *redirect, const char *const format, ...);
+
+#define rpgtk_log(type, message, ...)                                          \
+    rpgtk_log((rpgtk_log_metadata_t){__FILE__, __func__, __LINE__}, type,      \
+              nullptr, message __VA_OPT__(, ) __VA_ARGS__)
 
 #endif // TKLOGGING_MAIN_H
